c0bc844a79ede5bbc59acaa9be2321f83f63a429
unknown <bazhakk@gmail.com>
2023-06-14 16:17:02 +0300

    creating and styling a new section

---
+++ js/index.js
@@ -0,0 +1,19 @@
+jQuery(document).ready(function ( $ ) {
+
+  // Slick-slider
+  $('.slider').slick();
+  // $('.slider').slick({
+  //   dots: true,
+  //   infinite: true,
+  //   speed: 300,
+  //   slidesToShow: 1,
+  //   adaptiveHeight: true,
+  //   arrows: false,
+  //   dots: true
+  // });
+
+  // AOS init
+  AOS.init({
+    once: true
+  });
+});
